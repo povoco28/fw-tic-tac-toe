03c25017faaa58c0388fd372e98098a6a62f0d73
Paul Vo <233258021+povoco28@users.noreply.github.com>
2025-11-28 14:06:48 +0700

    Game working

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,7 +8,8 @@ function Game() {
 
   //Declaring a Winner
   useEffect(() => {
-    "Your code here";
+    const gameWinner = calculateWinner(squares);
+    setWinner(gameWinner);
   }, [squares]);
 
   //function to check if a player has won.
@@ -40,12 +41,18 @@ function Game() {
 
   //Handle player
   const handleClick = (i) => {
-    "Your code here";
+    if (winner || squares[i]) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
 
   //Restart game
-  const handlRestart = () => {
-    "Your code here";
+  const handleRestart = () => {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    setWinner(null);
   };
 
   return (
@@ -53,9 +60,9 @@ function Game() {
       <h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
       <div className="game">
         <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={"Your code here"} handleClick={"Your code here"} />
+        <Board squares={squares} handleClick={handleClick} />
       </div>
-      <button onClick={"Your code here"} className="restart-btn">
+      <button onClick={handleRestart} className="restart-btn">
         Restart
       </button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
